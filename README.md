fde9b8a452ea0fef2957233c40e9fd5600c30aa6
Roman <scher.roman@gmail.com>
2016-11-24 02:15:58 -0500

    Discarding segments with text, discarding black/white segments, -db debug option saves rejected segments
    - Text detection via tesseract OCR
    - Still testing if black/white detection via Pillow works

--- README.md
+++ README.md
@@ -4,33 +4,37 @@ A music video generator based on beat patterns
 
 Built with [essentia](https://github.com/MTG/essentia) and [moviepy](https://github.com/Zulko/moviepy)
 
-## Installation
+## Installation (Mac OS X)
 
 1 - [Install Miniconda](http://conda.pydata.org/miniconda.html) (A Python package manager)
 
 2 - [Install Homebrew](http://brew.sh/) (General purpose package manager for mac)
 
-3 - Create mvgen virtual environment
+3 - Install [tesseract](https://github.com/tesseract-ocr/tesseract)
+
+`brew install tesseract --with-all-languages`
+
+4 - Create mvgen virtual environment
 
 `conda env create -f environment.yml`
 
-4 - Activate mvgen environment
+5 - Activate mvgen environment
 
 `source activate mvgen`
 
-5 - Make sure most recent version of Xcode is installed
+6 - Make sure most recent version of Xcode is installed
 
-6 - [Install Essentia](http://essentia.upf.edu/documentation/installing.html) For OS X Via Homebrew 
+7 - [Install Essentia 2.1](http://essentia.upf.edu/documentation/installing.html) For OS X Via Homebrew 
 
-7 - Move Essentia package from Homebrew to your mvgen conda environment
+8 - Move Essentia package from Homebrew to your mvgen conda environment
 
 `cp -r /usr/local/lib/python2.7/site-packages/essentia /Users/myuser/miniconda/envs/mvgen/lib/python2.7/site-packages/essentia`
 
-8 - [Fix matplotlib](http://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
+9 - [Fix matplotlib](http://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
 
-9 - Download [scherroman/moviepy](https://github.com/scherroman/moviepy) from github.
+10 - Download [scherroman/moviepy](https://github.com/scherroman/moviepy) from github (forked from [Zulko/moviepy](https://github.com/Zulko/moviepy) with added fix [#225](https://github.com/Zulko/moviepy/pull/225)).
 
-10 - Install moviepy into mvgen conda environment (from downloaded moviepy directory)
+11 - Install moviepy into mvgen conda environment (from downloaded moviepy directory)
 
 `(sudo) python setup.py install`
 
--- mvgen/make_music_video.py
+++ mvgen/make_music_video.py
@@ -2,22 +2,32 @@ import os
 import sys
 import shutil
 import random
+import logging
 import argparse
+import tesserocr
 import essentia
 import essentia.standard
 from moviepy.editor import *
 from moviepy.video.tools.cuts import detect_scenes
 from tqdm import tqdm
+from PIL import Image
 import Tkinter as tk
 import tkFileDialog
 
+debug = False
 music_video_name = None
 
-OUTPUT_PATH_BASE = "output/"
-OUTPUT_NAME = "music_video"
+OUTPUT_PATH_BASE = 'output/'
+OUTPUT_NAME = 'music_video'
 OUTPUT_EXTENSION = '.mp4'
 
-SEGMENTS_PATH_BASE = "segments/"
+SEGMENTS_PATH_BASE = 'segments/'
+RS_PATH_BASE = 'rejected_segments/'
+RS_SCENE_CHANGE = RS_PATH_BASE + 'scene_change/'
+RS_TEXT_DETECTED = RS_PATH_BASE + 'text_detected/'
+RS_BLACK_OR_WHITE = RS_PATH_BASE + 'black_or_white/'
+
+# PRIMARY METHODS
 
 # Extracts beat locations and intervals from an audio file
 def get_beat_stats(audio_file):
@@ -31,13 +41,11 @@ def get_beat_stats(audio_file):
 		print(e)
 		sys.exit(1)
 	audio = loader()
-	# print audio
-
+	
 	# Get beat stats from audio
 	rhythm_loader = essentia.standard.RhythmExtractor2013()
 	rhythm = rhythm_loader(audio)
 	beat_stats = {'beat_locations':rhythm[1], 'beat_intervals':rhythm[4]}
-	# print beat_stats
 
 	return beat_stats
 
@@ -64,6 +72,7 @@ def get_video_segments(video_files, beat_stats):
 
 	# Extract video segments from videos
 	video_segments = []
+	rejected_segments = {'scene_change':[], 'text_detected':[], 'black_or_white':[]}
 	for beat_interval in tqdm(beat_stats['beat_intervals']):
 		video_segment = None
 		while video_segment == None:
@@ -71,21 +80,25 @@ def get_video_segments(video_files, beat_stats):
 			random_video = videos[random_video_number]
 			start_time = round(random.uniform(0, random_video.duration - beat_interval), 8)
 			end_time = start_time + round(beat_interval, 8)
-			# print "start {}".format(start_time)
-			# print "end {} \n".format(end_time)
+
+			video_segment = random_video.subclip(start_time,end_time)
 
 			# Discard video segment if there is a scene change
-			video_segment = random_video.subclip(start_time,end_time)
-			cuts, luminosities = detect_scenes(video_segment, fps=24, progress_bar=False)
-			# print("segment {} num scenes: {}".format(len(video_segments),len(cuts)))
-			if len(cuts) > 1:
+			if segment_contains_scene_change(video_segment):
+				rejected_segments['scene_change'].append(video_segment)
 				video_segment = None
+			# Discard video segment if there is any detectable text
+			elif segment_contains_text(video_segment):
+				rejected_segments['text_detected'].append(video_segment)
+				video_segment = None
+			# Discard video segment if only black or white
+			elif segment_is_black_or_white(video_segment):
+				rejected_segments['black_or_white'].append(video_segment)
+				video_segment = None
+
 		video_segments.append(video_segment)
 
-	# for video_segment in video_segments:
-	# 	print video_segment.duration
-
-	return video_segments
+	return video_segments, rejected_segments
 
 # Compile music video from video segments and audio
 def create_music_video(video_segments, audio_src):
@@ -118,6 +131,84 @@ def save_video_segments(video_segments):
 		segment_path = segments_dir + "%s" % count + OUTPUT_EXTENSION
 		video_segment.write_videofile(segment_path, fps=24, codec="libx264")
 		count += 1
+
+def save_rejected_segments(rejected_segments):
+	print "Saving rejected segments..."
+
+	if os.path.exists(RS_PATH_BASE):
+		shutil.rmtree(RS_PATH_BASE)
+	os.makedirs(RS_PATH_BASE)
+	os.makedirs(RS_SCENE_CHANGE)
+	os.makedirs(RS_TEXT_DETECTED)
+	os.makedirs(RS_BLACK_OR_WHITE)
+
+	count = 0
+	for rejected_segment in rejected_segments['scene_change']:
+		segment_path = RS_SCENE_CHANGE + "%s" % count + OUTPUT_EXTENSION
+		rejected_segment.write_videofile(segment_path, fps=24, codec="libx264")
+		count += 1
+
+	count = 0
+	for rejected_segment in rejected_segments['text_detected']:
+		segment_path = RS_TEXT_DETECTED + "%s" % count + OUTPUT_EXTENSION
+		rejected_segment.write_videofile(segment_path, fps=24, codec="libx264")
+		count += 1
+
+	count = 0
+	for rejected_segment in rejected_segments['black_or_white']:
+		segment_path = RS_BLACK_OR_WHITE + "%s" % count + OUTPUT_EXTENSION
+		rejected_segment.write_videofile(segment_path, fps=24, codec="libx264")
+		count += 1
+
+def print_rejected_segment_stats(rejected_segments):
+	print "# rejected segments with scene changes: {}".format(len(rejected_segments['scene_change']))
+	print "# rejected segments with text detected: {}".format(len(rejected_segments['text_detected']))
+	print "# rejected segments with black or white: {}".format(len(rejected_segments['black_or_white']))
+
+# HELPER METHODS
+
+def segment_contains_scene_change(video_segment):
+	cuts, luminosities = detect_scenes(video_segment, fps=24, progress_bar=False)
+	# print("segment {} num scenes: {}".format(len(video_segments),len(cuts)))
+	if len(cuts) > 1:
+		return True
+	else:
+		return False
+
+def segment_contains_text(video_segment):
+	#Check first frame
+	frame = video_segment.get_frame(t='00:00:00')
+	frame_image = Image.fromarray(frame)
+	text = tesserocr.image_to_text(frame_image)
+	if (len(text.strip()) > 0):
+		return True
+	#Check last frame
+	frame = video_segment.get_frame(t=video_segment.duration)
+	frame_image = Image.fromarray(frame)
+	text = tesserocr.image_to_text(frame_image)
+	if (len(text.strip()) > 0):
+		return True
+
+	return False
+
+def segment_is_black_or_white(video_segment):
+	first_frame_is_black_or_white = False
+	last_frame_is_black_or_white = False
+
+	first_frame = video_segment.get_frame(t='00:00:00')
+	first_frame_image = Image.fromarray(first_frame)
+	if sum(first_frame_image.convert("L").getextrema()) in (0, 2):
+		first_frame_is_black_or_white = True
+
+	last_frame = video_segment.get_frame(t=video_segment.duration)
+	last_frame_image = Image.fromarray(last_frame)
+	if sum(last_frame_image.convert("L").getextrema()) in (0, 2):
+		last_frame_is_black_or_white = True
+
+	if first_frame_is_black_or_white and last_frame_is_black_or_white:
+		return True
+	else:
+		return False
 		
 def get_output_path(music_video_name):
 	return OUTPUT_PATH_BASE + music_video_name + OUTPUT_EXTENSION
@@ -139,6 +230,8 @@ def listdir_nohidden(path):
         if not file.startswith('.'):
             yield path + file
 
+# MAIN
+
 def parse_args(args):
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-a', '--audio-source', dest='audio_src', help='The audio file for the music video. Supports any audio format supported by ffmpeg, such as wav, aiff, flac, ogg, mp3, etc...')
@@ -146,6 +239,7 @@ def parse_args(args):
 	parser.add_argument('-o', '--output-name', dest='output_name', help='The name for the music video. Otherwise will output music_video_0' + OUTPUT_EXTENSION + ', music_video_1' + OUTPUT_EXTENSION + ', etc...')
 	parser.add_argument('-im', '--input-manually', dest='input_manually', action='store_true', default=False, help='Pass in this argument to skip the file selection dialog and manually input the audio and video sources via the -a and -v arguments.')
 	parser.add_argument('-ss', '--save-segments', dest='save_segments', action='store_true', default=False, help='Pass in this argument to save all the individual segments that compose the music video.')
+	parser.add_argument('-db', '--debug', dest='debug', action='store_true', default=False, help='Pass in this argument to print debug statements and save all rejected segments.')
 	return parser.parse_args(args)
 
 if __name__ == '__main__':
@@ -156,6 +250,11 @@ if __name__ == '__main__':
 	video_src = args.video_src
 	output_name = args.output_name if args.output_name else None
 	save_segments = args.save_segments
+	debug = args.debug
+
+	# Setup debug logging
+	if debug:
+		logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 	# Set global name for music video
 	music_video_name = get_music_video_name(output_name)
@@ -204,21 +303,29 @@ if __name__ == '__main__':
 
 		video_files = list(video_src)
 
-	# print "audio_src {}".format(audio_src)
-	# print "video_src {}".format(video_src)
-	# print "video_files: {}".format(video_files)
-
+	logging.debug("audio_src {}".format(audio_src))
+	logging.debug("video_src {}".format(video_src))
+	logging.debug("video_files: {}".format(video_files))
+	
 	# Get beat locations and intervals from audio file
 	beat_stats = get_beat_stats(audio_src)
+	logging.debug("Beat intervals: {}".format(beat_stats['beat_intervals']))
 
 	# Generate video segments according to beat intervals in audio file
-	video_segments = get_video_segments(video_files, beat_stats)
+	video_segments, rejected_segments = get_video_segments(video_files, beat_stats)
 
 	# Compile music video from video segments and audio
 	create_music_video(video_segments, audio_src)
 
+	# Print stats for rejected video segments
+	print_rejected_segment_stats(rejected_segments)
+
 	# Save the individual segments if asked to do so
-	if save_video_segments:
+	if save_segments:
 		save_video_segments(video_segments)
+
+	# Save the rejected video segments if in debug mode
+	if debug:
+		save_rejected_segments(rejected_segments)
 
 
